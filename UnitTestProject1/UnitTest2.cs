3debc6dbf59135dd31b4eb588d2b6f4dddf3df96
206940 <206940@fa.edu.ru>
2023-10-03 15:18:58 +0300

    ;l

---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,20 @@
+﻿using Pelipenko220.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void AuthTest()
+        {
+            var page = new Authorization();
+            Assert.IsTrue(page.Auth("test", "test"));
+            Assert.IsFalse(page.Auth("user1", "12345"));
+            Assert.IsFalse(page.Auth("", ""));
+            Assert.IsFalse(page.Auth(" ", " "));
+        }
+    }
+}
